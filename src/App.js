2bee6560f7694fe2cf0e7c8de1380ed63862a4d9
smartpro1 <akeni.promise@yahoo.com>
2020-03-27 19:45:48 +0100

    set up done for register and login

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import Header from "./components/Layout/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import UpdateProject from "./components/Project/UpdateProject";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Register from "./components/UserManagement/Register";
+import Login from "./components/UserManagement/Login";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/addProject" component={AddProject} />
         <Route exact path="/updateProject/:id" component={UpdateProject} />
